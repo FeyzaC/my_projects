54031c8fe01679fa51ccef3be25d19e3f105bfab
gulfeyzaciftci <gulfeyza333@gmail.com>
2023-01-14 15:11:12 +0100

    if else

--- js/GuessTheNumber/app.js
+++ js/GuessTheNumber/app.js
@@ -4,4 +4,15 @@ const randomNumber = Math.round(Math.random() * 100);
 
 console.log(randomNumber);
 
-const guessNumber = Number(document.querySelector(".input").value);
+document.querySelector(".btn").addEventListener("click", () => {
+  const guessNumber = Number(document.querySelector(".input").value);
+  //   console.log(guessNumber);
+
+  if (randomNumber > guessNumber) {
+    alert("Up 👍");
+  } else if (randomNumber < guessNumber) {
+    alert("Down 👎");
+  } else {
+    alert("Perfect 😍");
+  }
+});
